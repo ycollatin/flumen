dc72fbad754639b22c3c6681fb950e204afd9566
Yves Ouvrard, alias Collatinus <yves.ouvrard@collatinus.org>
2020-03-25 19:58:15 +0100

    debog

--- mot.go
+++ mot.go
@@ -169,8 +169,8 @@ func (m *Mot) estNoyau(g *Groupe) bool {
 // Sub : pos string, morpho []string, accord string
 // gocol.Sr : Lem, Morphos []string
 func (m *Mot) estSub(sub *Sub, mn *Mot) bool {
-	//debog := m.gr=="rem" && mn.gr=="deligavit"// && sub.pos=="n"
-	//if debog {fmt.Println("    . estSub",m.gr, mn.gr, "sub.pos",sub.pos,"morpho",sub.morpho)}
+	//debog := m.gr=="prae" && mn.gr=="timore" && sub.pos=="prepAbl prepAA"
+	//if debog {fmt.Println("   .estSub",m.gr, mn.gr, "sub.pos-"+sub.pos+"-morpho",sub.morpho)}
 	var respos, resmorf gocol.Res
 	// le sub a-t-il le bon pos ?
 	//if debog {fmt.Println("    estSub, sub.terminal",sub.terminal)}
