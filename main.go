cc9994ab3bad78e046fb6c6e1e53ccbb5fddaf1a
Yves Ouvrard, alias Collatinus <yves.ouvrard@collatinus.org>
2020-04-03 21:50:26 +0200

    indic de branche

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 
 package main
 
+// XXX branche lm
 // FIXME la vérif estSub choisit la première solution possible. 
 //		 Et si une autre est la bonne ? cf. TODO 2
 //		Création donc de la branche multi.
